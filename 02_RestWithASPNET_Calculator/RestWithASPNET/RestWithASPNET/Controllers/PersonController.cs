889aad6eb91f431e394d938ada940d4d73dbb870
Yuri Fernandes <yuri.fernandes@primecontrol.com.br>
2021-07-14 15:53:38 -0300

    Working four verbs

--- 02_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -9,11 +9,11 @@ namespace RestWithASPNET.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CalculatorController : ControllerBase
+    public class PersonController : ControllerBase
     {
-        private readonly ILogger<CalculatorController> _logger;
+        private readonly ILogger<PersonController> _logger;
 
-        public CalculatorController(ILogger<CalculatorController> logger)
+        public PersonController(ILogger<PersonController> logger)
         {
             _logger = logger;
         }
